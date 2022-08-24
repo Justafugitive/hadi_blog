9d2e9df5438ddf6e2cc599047236e0cd5c332d83
Justafugitive <Justafugitive>
2022-08-24 16:59:04 +0200

    Can now post a comment to graphcms

--- components/Author.tsx
+++ components/Author.tsx
@@ -9,8 +9,6 @@ const Author = ({ author }:AuthorProps) => {
   return (
     <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
       <div className="absolute left-0 right-0 -top-14">
-
-      </div>
         <Image
           alt={author.name}
           unoptimized
@@ -19,8 +17,9 @@ const Author = ({ author }:AuthorProps) => {
           className="align-middle rounded-full"
           src={author.photo.url}
         />
-        <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
-        <p className="text-white text-lg">{author.bio}</p>
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
     </div>
   )
 }
