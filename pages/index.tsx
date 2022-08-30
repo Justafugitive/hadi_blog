19463635edc4f061e506f8af23a402aa3fd1962e
Justafugitive <Justafugitive>
2022-08-30 15:02:44 +0200

    Now can get updated post

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage<Props> = ({ posts }) => {
       <FeaturedPosts />
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className="lg:col-span-8 col-span-1">
-        {posts.map((post ) => <PostCard post={post.node} key={post.title} />)}
+        {posts.map((post, index ) => <PostCard post={post.node} key={index} />)}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
