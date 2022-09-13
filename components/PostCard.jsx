a984b528bc17f8aa510717cce9c4dee7aee87f3f
Justafugitive <Justafugitive>
2022-09-13 09:21:00 +0200

    changed index from tsx to js

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -9,7 +9,6 @@ const PostCard = ({ post }) => (
     {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
       <Image
         unoptimized
-        loader={grpahCMSImageLoader}
         alt={post.title}
         className="shadow-lg rounded-t-lg lg:rounded-lg"
         layout="fill"
