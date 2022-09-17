5afbc9a57f4538cd2902f9aa8687e14404bba0ca
Justafugitive <Justafugitive>
2022-09-17 20:25:58 +0200

    added head title

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ import { getPosts } from '../services';
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
+    <Head>
+        <title>hadi</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
